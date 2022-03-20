c86082424eacc38c2e3d18dd2ae88ea40dfe4981
vlagru <v.grulich@seznam.cz>
2022-03-20 10:15:53 +0100

    Add the new class of Board and others

--- src/thedrake/TroopTile.java
+++ src/thedrake/TroopTile.java
@@ -15,39 +15,35 @@ public class TroopTile implements Tile {
     // Vrací True, pokud je tato dlaždice volná a lze na ni vstoupit
     @Override
     public boolean canStepOn() {
-        if (troop) {
-            return true;
-        } return false;
+        return true;
     }
 
     // Vrací True, pokud tato dlaždice obsahuje jednotku
     @Override
     public boolean hasTroop() {
-        if (!troop.equals(null)) {
-            return true;
-        } return false;
+        return true;
     }
+
     // Vrací barvu, za kterou hraje jednotka na této dlaždici
-    public PlayingSide side() {
-        if (face.equals(PlayingSide.BLUE)) { // face neporovnatelne s barvami
-            return PlayingSide.BLUE;
-        } else return PlayingSide.ORANGE;
+    public PlayingSide side() { // face neporovnatelne s barvami
+            return side;
     }
 
     // Vrací stranu, na kterou je jednotka otočena // getter
     public TroopFace face() {
-        if (TroopFace.AVERS.equals(troop)) { // blbost, neporovnatelne
-            return TroopFace.AVERS;
-        } return TroopFace.REVERS;
+        return face;
     }
 
     // Jednotka, která stojí na této dlaždici
-    public Troop troop()
+    public Troop troop() {
+        return troop;
+    }
 
     // Vytvoří novou dlaždici, s jednotkou otočenou na opačnou stranu
 // (z rubu na líc nebo z líce na rub)
     public TroopTile flipped() {
-        if (face.equals(TroopFace.AVERS))
+        if (face == TroopFace.AVERS) {
             return new TroopTile(troop, side, TroopFace.REVERS);
+        } return new TroopTile(troop, side, TroopFace.AVERS);
     }
 }
